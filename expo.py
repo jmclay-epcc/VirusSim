98e2620c1b714d16dcecdf486fecfca8a523c772
james mclay <j.mclay@epcc.ed.ac.uk>
2024-05-23 11:30:39 +0100

    Minor changes to expo.py

--- expo.py
+++ expo.py
@@ -8,7 +8,7 @@ plt.subplots_adjust(left=0.1, bottom=0.35)
 ax.set_xlim([0, 300])
 ax.set_ylim([0, 105])
 
-plt.xlabel('Distance from infected player (unit)')
+plt.xlabel('Distance from centre of infected player (unit)')
 plt.ylabel('Chance of being infected (%)')
 plt.title('Infection chance curves')
 plt.grid(True)
@@ -20,13 +20,13 @@ k = ((np.square(I-0.5)/1.25) + 0.05) * (150/M)
 x0 = (0.8*M*(I)) + 0.1*M
 Y =  100 / (1 + np.exp(k*(X-x0)))
 
-line, = ax.plot(X, Y, lw=2, color="purple")
+line, = ax.plot(X, Y, lw=3, color="purple")
 
 axMaxRange = plt.axes([0.25, 0.2, 0.6, 0.03])
-sMaxRange = Slider(axMaxRange, 'Infectious Range', 0.1, 300.0, valinit=M)
+sMaxRange = Slider(axMaxRange, 'Infectious Range', 1, 300.0, valinit=M)
 
 axInfStrength = plt.axes([0.25, 0.05, 0.6, 0.03])
-sInfStrength = Slider(axInfStrength, 'Infection Strength', 0.001, 1.0, valinit=I)
+sInfStrength = Slider(axInfStrength, 'Infection Strength', 0.01, 1.0, valinit=I)
 
 def update(val):
     M = int(sMaxRange.val)
